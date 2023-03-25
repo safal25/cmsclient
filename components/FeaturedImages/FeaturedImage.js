95ca94eff7526263786bc692e1ab95ea26dc716a
Safal Khampariya <safal.khampariya@lntinfotech.com>
2023-03-25 13:09:37 +0530

    Added functionality to show the existing images in featured image section so that user can choose from existing images and also delete images

--- components/FeaturedImages/FeaturedImage.js
+++ components/FeaturedImages/FeaturedImage.js
@@ -1,17 +1,57 @@
-import { InboxOutlined } from '@ant-design/icons';
-import { message, Upload } from 'antd';
-import { useContext } from 'react';
+import { InboxOutlined,CloseCircleOutlined } from '@ant-design/icons';
+import { message, Upload,Image,Badge } from 'antd';
+import { useContext,useEffect, useState } from 'react';
 import { AuthContext } from '../../context/auth';
 import { MediaContext } from '../../context/media';
-
+import axios from 'axios';
 
 const {Dragger}=Upload;
 
 
 const FeaturedImage=()=>{
 
+    //state
+    const [showPreview,setShowPreview]=useState(false);
+
+    //context
     const [auth,setAuth]=useContext(AuthContext);
     const {media,setMedia}=useContext(MediaContext);
+    
+
+    const getImages=async ()=>{
+        try {
+
+          const {data}=await axios.get('/media/get-images');
+
+          if(data?.success){
+            setMedia({...media,Images : data?.images});
+          }
+          else{
+            console.log('Error in fetching images');
+          }
+
+        } catch (error) {
+            console.log('Error in fetching images',error);
+        }
+    }
+
+    useEffect(()=>{
+      getImages();
+    },[]);
+
+    const deleteImage=async (id)=>{
+      try {
+
+        const {data}=await axios.delete(`/media/delete-image/${id}`)
+        if(data?.success){
+          setMedia({...media,Images : media.Images.filter((image)=>{return image._id!==id})});
+        }
+        
+      } catch (error) {
+          console.log('Error while deleting images',error);
+      }
+    }
+
 
     const props = {
         name: 'file',
@@ -44,12 +84,31 @@ const FeaturedImage=()=>{
 
 
       return (
-        <Dragger {...props}>
-            <p className="ant-upload-drag-icon">
-                <InboxOutlined />
-            </p>
-            <p>Click or drag file to this area to upload</p>
-        </Dragger>
+        <>
+          <Dragger {...props}>
+              <p className="ant-upload-drag-icon">
+                  <InboxOutlined />
+              </p>
+              <p>Click or drag file to this area to upload</p>
+          </Dragger>
+          <div style={{textAlign : 'center'}}>
+              {media?.Images.length!==0 && (
+                  media.Images.map((img)=>{
+                   return <Badge>
+                     <Image  preview={showPreview} src={img.url} style={{
+                      paddingTop : '5px',
+                      paddingRight : '10px',
+                      height : '100px',
+                      width : '100px',
+                      objectFit : 'cover',
+                    }} onClick={()=>(setMedia({...media,selected : {image : img}}))}/>
+                     <br/>
+                     <CloseCircleOutlined style={{color : '#f5222d'}} onClick={()=>{deleteImage(img._id)}}/>
+                    </Badge>
+                  })
+              )}
+          </div>
+        </>
       );
 
 }
